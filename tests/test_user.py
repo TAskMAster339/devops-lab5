7d0be9ad3c4727e675d38d17a7da9635e892f5f8
TAskMAster339 <dimagr3214@gmail.com>
2025-04-15 22:03:52 +0300

    feat: github actions

--- tests/test_user.py
+++ tests/test_user.py
@@ -18,24 +18,59 @@ users = [
     }
 ]
 
+
 def test_get_existed_user():
     '''Получение существующего пользователя'''
     response = client.get("/api/v1/user", params={'email': users[0]['email']})
     assert response.status_code == 200
     assert response.json() == users[0]
 
+
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={"email": "helafasfa@gmail.com"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+
+    new_user = {
+        "name": "test",
+        "email": "test@gmail.com",
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={
+        "name": users[0]["name"],
+        "email": users[0]["email"],
+        }
+    )
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
+
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={
+        'email': users[1]['email'],
+        }
+    )
+    assert response.status_code == 200
+    response = client.delete("/api/v1/user", params={
+        'email': users[1]['email'],
+        }
+    )
+    assert response.status_code == 204
+    response = client.get("/api/v1/user", params={
+        'email': users[1]['email'],
+        }
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
